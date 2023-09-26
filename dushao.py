1e9292a5baa65543d2b245e306f01e7fe42376ac
woonrie <woonrie@gmail.com>
2023-09-26 18:07:21 -0400

    fix

--- dushao.py
+++ dushao.py
@@ -1,5 +1,9 @@
+
 import random as ra
 import time as t
+from git import Repo
+import os
+
 # method to get n distinct nums between 0 and m (both included)
 def req(n, m):
     if n == 0 or m == 0 or n > m + 1:
@@ -30,22 +34,74 @@ ns = {
         10 : 'shi2'
     }  
 
+def sbm(scst):
+    # Clone the repository
+    repo_dir = "/Users/wnr/bai/dushao/"
+
+    repo_url = "https://github.com/seeohh/dushao.git"
+    if not os.path.exists(repo_dir):
+        repo = Repo.clone_from(repo_url, repo_dir)
+    else:
+        repo = Repo(repo_dir)
+    
+    # Pull the latest changes
+    origin = repo.remotes.origin
+    origin.pull()
+    
+    # Append the new score to scores.txt
+    scores_file = os.path.join(repo_dir, "scores.txt")
+    with open(scores_file, "a") as f:
+        f.write(f"{scst}\n")
+    
+    # Commit the changes
+    repo.git.add(scores_file)
+    repo.git.commit('-m', 'Add new score')
+    
+    # Push the changes
+    origin.push()
+
 def ses():
     m = 11
     while m > 10:
         m = int(input("what number should we go to? (max 10) : "))
 
     sm = 0
+    sc = 0
+
     ord = req(m+1,m) 
     for n in ord:   
         print(f"{n} : ")
         st = t.time()  
-        input("enter : ")
+        g = input("ans : ")
+        if n == 1 and g == "yao1" or g == "yi1":
+            sc += 1
+        if g == ns[n]:
+            sc += 1
         et = t.time() - st 
         sm += et
         print(ns[n])
-        input("enter : ")
-    print(round(sm, 3), " seconds.")
+        input("next : ")
+    
+    et -= m-sc
+    score = sc * et
+    print(round(sm, 3), " seconds.\n")
+    print("score:", round(score, 3))
+
+    tt = t.localtime()
+    with open("scores.txt", "w") as f:
+        wr = f.write(f"max: {m}         {round(score, 3)}         {tt}\n")
+
+    chc = input("submit score? (y/n) : ")
+    if chc == "Y" or chc == "y":
+        sbm(wr)
+        print("score submitted.\n")
+    
+    chcc = input("again? (y/n) : ")
+    if chcc == "Y" or chcc == "y":
+        ses()
+    else:
+        print("bye.")
+
+
 
 ses()
-
